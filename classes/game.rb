2492fe80f0e3d818b04009f266ad392160974279
Robert Shum <robertshum@gmail.com>
2023-11-13 19:49:41 -0800

    Add remove life fn.

--- classes/game.rb
+++ classes/game.rb
@@ -8,7 +8,7 @@ class Game
     player1 = Player.new('Player 1')
     player1.who_am_i
 
-    player2 = Player.new('Player 2')
-    player2.who_am_i
+    # player2 = Player.new('Player 2')
+    # player2.who_am_i
   end
 end
--- classes/player.rb
+++ classes/player.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
-# playe info
+PLAYER_LIVES = 3
+
+# player info
 class Player
   def initialize(name)
     @name = name
+    @life = PLAYER_LIVES
   end
 
-  attr_reader :name
+  attr_reader :name, :life
+
+  def lose_life
+    @life = life - 1
+  end
 
   def who_am_i
     puts "I am #{name}"
